6fe7ed7b08eb82cb7ed55457fe1a2bee87d6024b
siva-n22 <namalasiva22@gmail.com>
2021-02-20 19:14:32 +0530

    initial commit

---
+++ script.py
@@ -0,0 +1,24 @@
+import sys
+
+import requests
+
+
+print('\n', sys.version)
+print(sys.executable)
+
+
+def greet(who_to_greet, hr):
+    return f'Hello! {who_to_greet}' + f' {hr}'
+
+
+print(greet('siva', 4))
+print('hello')
+# requests.get('')
+# heyfygfyrgfy
+
+
+def dad():
+    return 'hello'
+
+
+print(dad())
